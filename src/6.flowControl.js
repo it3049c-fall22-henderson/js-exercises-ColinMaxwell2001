4bd8e2d498ba88582e81d86d51895b30f52af971
ColinMaxwell2001 <95194573+ColinMaxwell2001@users.noreply.github.com>
2022-09-02 19:23:34 -0400

    finished flowControl methods

--- src/6.flowControl.js
+++ src/6.flowControl.js
@@ -8,6 +8,25 @@
  */
 function fizzBuzz(num) {
   
+  if (num % 15 ==0)
+    return "fizzbuzz";
+  
+  else if (num % 3 == 0)
+  {
+    return "fizz";
+  }
+  else if (num % 5 == 0)
+  {
+    return "buzz";
+  }
+  else if (typeof(num) === 'number')
+  {
+    return num;
+  }
+  else
+  {
+    return false;
+  }
 }
 
 module.exports = {
