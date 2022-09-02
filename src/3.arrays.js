d75a07e3cf114fe6a51e63c98fecbbe4a007bcba
ColinMaxwell2001 <95194573+ColinMaxwell2001@users.noreply.github.com>
2022-09-02 16:04:24 -0400

    finished sum array

--- src/3.arrays.js
+++ src/3.arrays.js
@@ -12,7 +12,13 @@ function indexOf(arr, item) {
 // Sum: you should be able to sum the items of an array
 // Example: arr = [1,2,3]   =>  6
 function sum(arr) {
-  return ;
+  let sum1 =0;
+
+  for (let i =0; i< arr.length; i++)
+  {
+    sum1 += arr[i];
+  }
+  return sum1;
 }
 
 // Filter: Should filter out all instances of a value from an array
