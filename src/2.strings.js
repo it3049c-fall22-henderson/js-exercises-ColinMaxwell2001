6049588bdbde7e3be0c409d306250d75d0c92546
ColinMaxwell2001 <95194573+ColinMaxwell2001@users.noreply.github.com>
2022-09-02 18:16:17 -0400

    finished reverseString, capitalize, and splitString

--- src/2.strings.js
+++ src/2.strings.js
@@ -9,13 +9,17 @@ function reverseString(str) {
    * 
    * Can even be done on one line
    */
-  return ;
+
+  var splitString = str.split("");
+  var reverseArray = splitString.reverse();
+  var joinArray = reverseArray.join("");
+  return joinArray;
 }
 
 //capitalize: should return the input in all-caps
 // Example: "this is a string" => "THIS IS A STRING"
 function capitalize(str) {
-  return ;
+  return str.toUpperCase();
 }
 
 // splitString: should divide a string into substrings and return an array
--- src/3.arrays.js
+++ src/3.arrays.js
@@ -60,7 +60,7 @@ function insert(arr, item, index) {
 // hint: use .map
 // Example: [1,2,4,5] => [1,4,16,25]
 function square(arr) {
-  return ;
+  return arr.map(x => x * x);
 }
 
 module.exports = {
